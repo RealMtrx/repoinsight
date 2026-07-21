7659d984bb76567440543a78d06c7901aae4fd19
Mtrx-Gxy <mtrxturki@gmail.com>
2026-07-21 04:19:18 +0300

    chore: bump version to 1.3.0

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "repoinsight";
-export const APP_VERSION = "1.2.0";
+export const APP_VERSION = "1.3.0";
 export const APP_DESCRIPTION = "Understand any repository in seconds.";
 
 export const DEFAULT_EXCLUDE_PATTERNS = [
